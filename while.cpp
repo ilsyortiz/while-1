0b9f7e47c6517a61382be617b39964bc32bcdca2
ilsyortiz <139251404+ilsyortiz@users.noreply.github.com>
2023-07-18 09:33:53 -0600

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,14 @@
+//WHILE ciclo repetitivo//
+//Ilsy Alessandra Ortíz Castañeda//
+#include <iostream>
+using namespace std;
+int main () {int numero;
+  cout<<"Ingrese un valor ";
+  cin>>numero;
+  while (numero <=100)
+  {
+      cout<<"Ingrese un valor ";
+      cin>> numero;
+  }
+  return 0;
+}
